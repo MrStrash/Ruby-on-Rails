43f5d838eec12aad08c7d9bbae6b5f90fd3ab506
MrStrash <MrStrash@i.ua>
2016-02-02 20:25:51 +0200

    Destroy

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,12 +6,15 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def edit
     @article = Article.find(params[:id])
   end
+
   def create
     @article = Article.new(article_params)
 
@@ -21,6 +24,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
   def update
     @article = Article.find(params[:id])
 
@@ -30,9 +34,16 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
 end
